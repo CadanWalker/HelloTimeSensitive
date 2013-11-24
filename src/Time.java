a83c977e96c246f63ee47db897afff8e1248386a
Cadan Walker <cadanw@hotmail.com>
2013-11-24 19:36:55 +0000

    Time class now has required methods to get the time from the system and to set it to a variable.

--- src/Time.java
+++ src/Time.java
@@ -1,5 +1,21 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
-//This is the class to check the time against the stored values.
+//This is the class to get the time from the computer
 public class Time {
+	
+	//This variable stores the time as a string.
+	private String time;
 
+	//This method sets the time to the variable 'time' when called.
+	public void getTime() {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+		Calendar cal = Calendar.getInstance();
+		time = dateFormat.format(cal.getTime());
+	}
+	
+	//This method returns the value stored in the variable 'time' when called.
+	public String giveTime() {
+		return time;
+	}
 }
